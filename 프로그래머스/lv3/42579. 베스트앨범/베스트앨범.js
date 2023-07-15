748f52a5c167b9cc46da8c4b29d7ee63b43ad2b7
LEE-YO-HAN <85297720+LEE-YO-HAN@users.noreply.github.com>
2023-07-16 03:39:40 +0900

    Update 베스트앨범.js

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
@@ -1,87 +1,56 @@
 function solution(genres, plays) {
-//     let sum = {}
+    let sum = {}
     
-//     genres.forEach((v,i)=>{
-//         if(sum[v]===undefined) {
-//             sum[v] = plays[i]
-//         } else {
-//             sum[v] += plays[i]
-//         }
-//     })
-    
-//     const sumKeys = Object.keys(sum)
-//     const sumValues = Object.values(sum)
-//     const sumArr = sumKeys.map((v,i)=>{
-//         return [v,sumValues[i]]
-//     }).sort((a,b)=>b[1]-a[1])
-    
-//     let albums = {}
-    
-//     sumArr.forEach(v=>{
-//         albums[v[0]] = []
-//     })
-    
-//     let albumArr = genres.map((v,i)=>{
-//         return [v,i,plays[i]]
-//     })
-    
-//     albumArr.sort((a,b)=>b[2]-a[2])
-    
-//     albumArr.forEach((v,i)=>{
-//         albums[v[0]].push(v[1])
-//     })
-    
-//     let result = ""
-//     Object.values(albums).forEach(v=>{
-//         v.forEach((v,i)=>{
-//             if(i<2){
-//                 result+=v
-//             }
-//         })
-//     })
-//     result = result.split("").map(Number)
-//     return result;
-    
-    let sum = {};
-    let songInfo = {};
-
-    for (let i = 0; i < genres.length; i++) {
-        const genre = genres[i];
-        const play = plays[i];
-
-        if (!sum[genre]) {
-            sum[genre] = play;
+    genres.forEach((v,i)=>{
+        if(sum[v]===undefined) {
+            sum[v] = plays[i]
         } else {
-            sum[genre] += play;
+            sum[v] += plays[i]
         }
-
-        if (!songInfo[genre]) {
-            songInfo[genre] = [];
-        }
-
-        songInfo[genre].push({
-            id: i,
-            play: play
-        });
-    }
-
-    const sortedGenres = Object.keys(sum).sort(
-        (a, b) => sum[b] - sum[a]
-    );
-
-    const result = [];
-
-    sortedGenres.forEach(v => {
-        songInfo[v].sort((a, b) => {
-            if (a.play === b.play) {
-                return a.id - b.id;
+    })
+    
+    const sumKeys = Object.keys(sum)
+    const sumValues = Object.values(sum)
+    
+    const sumArr = sumKeys.map((v,i)=>{
+        return [v,sumValues[i]]
+    }).sort((a,b)=>b[1]-a[1])
+    
+    let albums = {}
+    
+    sumArr.forEach(v=>{
+        albums[v[0]] = []
+    })
+    
+    let albumArr = genres.map((v,i)=>{
+        return [v,i,plays[i]]
+    })
+    
+    albumArr.sort((a,b)=>b[2]-a[2])
+    
+    sumArr.forEach((v,i)=>{
+        const value = v[0]
+        let genreFilter = albumArr.filter(item=>item[0]===value)
+        genreFilter.sort((a,b)=>{
+            if(a[2]===b[2]){
+                return a[1]-b[1]
             }
-            return b.play - a.play;
-        });
-
-        const selectedSongs = songInfo[v].slice(0, 2).map(song => song.id);
-        result.push(...selectedSongs);
-    });
-
+            return 
+        })
+        
+        genreFilter.forEach(item=>{
+           albums[value].push(item[1]) 
+        })
+       
+    })
+    
+    let result = []
+    Object.values(albums).forEach(item=>{
+        item.forEach((v,i)=>{
+            if(i<2){
+                result.push(v)
+            }
+        })
+    })
     return result;
 }
